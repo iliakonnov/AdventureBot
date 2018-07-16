304b3a00883044cdfd5c1948d994c777d793f7bf
Ilia Konnov <iliakonnov@gmail.com>
2018-07-16 17:09:38 +0300

    New room: Apple tree

---
+++ Content/Items/Notebook.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using AdventureBot;
+using AdventureBot.Item;
+using AdventureBot.ObjectManager;
+using AdventureBot.User;
+using AdventureBot.User.Stats;
+
+namespace Content.Items
+{
+    [Item("appletree/notebook")]
+    public class Notebook : ItemBase
+    {
+        public override Flag<BuyGroup> Group => new Flag<BuyGroup>();
+        public override string Name => "Дневник Ньютона";
+        public override string Description => string.Empty;
+        public override decimal? Price => null;
+        public override string Identifier => "appletree/notebook";
+
+        public override StatsEffect Effect => new StatsEffect(ChangeType.Add, new Dictionary<StatsProperty, decimal>
+        {
+            {StatsProperty.Intelligence, 5}
+        });
+
+        public override bool CanUse(User user, ItemInfo info)
+        {
+            return false;
+        }
+    }
+}
---
+++ Content/Rooms/AppleTree.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using AdventureBot;
+using AdventureBot.Item;
+using AdventureBot.Messenger;
+using AdventureBot.Room;
+using AdventureBot.User;
+
+namespace Content.Rooms
+{
+    [Available("room/appletree")]
+    public class AppleTree : RoomBase
+    {
+        public AppleTree()
+        {
+            Buttons = new NullableDictionary<MessageRecived, Dictionary<string, MessageRecived>>
+            {
+                {
+                    null, new Dictionary<string, MessageRecived>
+                    {
+                        {
+                            "Залезть наверх", (user, message) =>
+                            {
+                                SendMessage(
+                                    user,
+                                    "Ты стал карабкаться по веткам и случайно сбил одно яблоко. Оно упало на голову какому-то чудаковатому англичанина, сидевшему в тени ветвей. Вместо того, чтобы посмотреть наверх, он записал что-то в своей тетради и убежал куда-то в сторону Кембриджа. Тетрадь он оставил под яблоней."
+                                );
+                                user.ItemManager.Add(new ItemInfo("appletree/notebook", 1));
+                                user.RoomManager.Leave();
+                            }
+                        },
+                        {"Уйти", (user, message) => user.RoomManager.Leave()}
+                    }
+                }
+            };
+        }
+
+        public override string Name => "Яблоня";
+        public override string Identifier => "room/appletree";
+
+        public override void OnEnter(User user)
+        {
+            SendMessage(
+                user,
+                "У тебя возникло непреодолимое желание отведать яблок, но они висят слишком высоко.",
+                GetButtons(user)
+            );
+        }
+
+        public override bool OnLeave(User user)
+        {
+            return true;
+        }
+
+        public override void OnMessage(User user, RecivedMessage message)
+        {
+            HandleButtonAlways(user, message);
+        }
+    }
+}
