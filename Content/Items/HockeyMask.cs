697058c2a8adea551a7f69ee507c8b234bbc28b5
Ilia Konnov <iliakonnov@gmail.com>
2018-07-16 16:41:41 +0300

    New monster: Jason Voorhees

---
+++ Content/Items/HockeyMask.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using AdventureBot;
+using AdventureBot.Item;
+using AdventureBot.ObjectManager;
+using AdventureBot.User;
+using AdventureBot.User.Stats;
+
+namespace Content.Items
+{
+    [Item("jason/mask")]
+    public class HockeyMask : ItemBase
+    {
+        public override Flag<BuyGroup> Group => new Flag<BuyGroup>();
+        public override string Name => "Хоккейная маска";
+        public override string Description => string.Empty;
+        public override decimal? Price => null;
+        public override string Identifier => "jason/mask";
+
+        // TODO: Defence
+        public override StatsEffect Effect => new StatsEffect(ChangeType.Set, new Dictionary<StatsProperty, decimal>());
+
+        public override bool CanUse(User user, ItemInfo info)
+        {
+            return false;
+        }
+    }
+}
---
+++ Content/Rooms/Jason.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using AdventureBot.Item;
+using AdventureBot.Room;
+using AdventureBot.User;
+
+namespace Content.Rooms
+{
+    // Disabled
+    [Room("monster/jason")]
+    public class Jason : MonsterBase
+    {
+        public override string Name => "Джейсон Вурхиз";
+        public override string Identifier => "moster/jason";
+
+        public override decimal Health
+        {
+            get
+            {
+                if (IsFriday()) return 1500 * 2;
+
+                return 1500;
+            }
+        }
+
+        private static bool IsFriday()
+        {
+            var today = DateTime.Now.Date;
+            return today.DayOfWeek == DayOfWeek.Friday && today.Day == 13;
+        }
+
+        public override decimal GetDamage(User user)
+        {
+            if (IsFriday()) return 45 * 2;
+
+            return 45;
+        }
+
+        public override void Enter(User user, string[][] buttons)
+        {
+            SendMessage(
+                user,
+                IsFriday()
+                    ? "Сегодня его день, и он будет убивать!"
+                    : "Сегодня не его день, и он очень зол!",
+                buttons
+            );
+        }
+
+        public override bool OnRunaway(User user)
+        {
+            return true;
+        }
+
+        public override void OnWon(User user)
+        {
+            user.ItemManager.Add(new ItemInfo("jason/mask", 1));
+        }
+    }
+}
