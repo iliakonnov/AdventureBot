bf6480caa46a219f381ca4b0e6366cc64fba1228
Ilia Konnov <iliakonnov@gmail.com>
2018-07-16 16:12:20 +0300

    New room: Worms

---
+++ Content/Items/Worm.cs
@@ -0,0 +1,20 @@
+﻿using AdventureBot;
+using AdventureBot.Item;
+using AdventureBot.ObjectManager;
+using AdventureBot.User;
+using AdventureBot.User.Stats;
+
+namespace Content.Items
+{
+    [Item("worms/worm")]
+    public class Worm : ItemBase
+    {
+        public override Flag<BuyGroup> Group => new Flag<BuyGroup>(BuyGroup.Market);
+        public override string Name => "Червяк";
+        public override string Description => "Маленький, но храбрый";
+        public override decimal? Price => 1m;
+        public override string Identifier => "worms/worm";
+        public override StatsEffect Effect => null;
+        public override bool CanUse(User user, ItemInfo info) => false;
+    }
+}
---
+++ Content/Rooms/Worms.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using AdventureBot;
+using AdventureBot.Item;
+using AdventureBot.Messenger;
+using AdventureBot.Room;
+using AdventureBot.User;
+
+namespace Content.Rooms
+{
+    [Available("room/worms")]
+    public class Worms : RoomBase
+    {
+        public override string Name => "Террариум";
+        public override string Identifier => "room/worms";
+
+        public Worms()
+        {
+            Buttons = new NullableDictionary<MessageRecived, Dictionary<string, MessageRecived>>
+            {
+                {
+                    null, new Dictionary<string, MessageRecived>
+                    {
+                        {
+                            "Уйти", (user, message) => { user.RoomManager.Leave(); }
+                        }
+                    }
+                }
+            };
+        }
+
+        public override void OnEnter(User user)
+        {
+            SendMessage(
+                user,
+                "В террариуме оказалось несколько червяков, палящих друг по другу миниатюрными, но ничуть не менее опасными базуками.",
+                GetButtons(user)
+            );
+        }
+
+        public override bool OnLeave(User user)
+        {
+            user.ItemManager.Add(new ItemInfo("worms/worm", 3));
+            SendMessage(user, "Перед уходом ты подобрал бездыханные тела бравых бойцов.");
+            return true;
+        }
+
+        public override void OnMessage(User user, RecivedMessage message)
+        {
+            HandleButtonAlways(user, message);
+        }
+    }
+}
