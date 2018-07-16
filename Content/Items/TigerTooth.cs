81fbba312960b14ccbfa4f580d8c565b91679f84
Ilia Konnov <iliakonnov@gmail.com>
2018-07-16 18:38:45 +0300

    New monster: Tiger

---
+++ Content/Items/TigerTooth.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using AdventureBot;
+using AdventureBot.Item;
+using AdventureBot.ObjectManager;
+using AdventureBot.User;
+using AdventureBot.User.Stats;
+
+namespace Content.Items
+{
+    [Item("tiger/tooth")]
+    public class TigerTooth : ItemBase
+    {
+        public override Flag<BuyGroup> Group => new Flag<BuyGroup>();
+        public override string Name => "Клык тигра";
+        public override string Description => "Ты с трудом добыл его с невымершого тигра";
+        public override decimal? Price => null;
+        public override string Identifier => "tiger/tooth";
+
+        public override StatsEffect Effect => new StatsEffect(ChangeType.Add, new Dictionary<StatsProperty, decimal>
+        {
+            {StatsProperty.Strength, 3}
+        });
+
+        public override bool CanUse(User user, ItemInfo info)
+        {
+            return false;
+        }
+    }
+}
---
+++ Content/Rooms/Tiger.cs
@@ -0,0 +1,34 @@
+﻿using AdventureBot.Item;
+using AdventureBot.Room;
+using AdventureBot.User;
+
+namespace Content.Rooms
+{
+    [Available("monster/tiger")]
+    public class Tiger : MonsterBase
+    {
+        public override string Name => "Саблезубый тигр";
+        public override string Identifier => "monster/tiger";
+        public override decimal Health => 75;
+
+        public override decimal GetDamage(User user)
+        {
+            return 10;
+        }
+
+        public override void Enter(User user, string[][] buttons)
+        {
+            SendMessage(user, "Разве они не вымерли?", buttons);
+        }
+
+        public override bool OnRunaway(User user)
+        {
+            return true;
+        }
+
+        public override void OnWon(User user)
+        {
+            user.ItemManager.Add(new ItemInfo("tiger/tooth", 1));
+        }
+    }
+}
