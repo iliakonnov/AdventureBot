b3ecaa8fe67e5f832231a673f01269185fb582f8
Ilia Konnov <iliakonnov@gmail.com>
2018-07-16 13:17:58 +0300

    New room and item

---
+++ Content/Items/Chainsword.cs
@@ -0,0 +1,80 @@
+﻿using AdventureBot;
+using AdventureBot.Item;
+using AdventureBot.Messenger;
+using AdventureBot.ObjectManager;
+using AdventureBot.Room;
+using AdventureBot.User;
+using AdventureBot.User.Stats;
+
+namespace Content.Items
+{
+    [Item("spacemarine/chainsword")]
+    public class Chainsword : ItemBase
+    {
+        public override string Name => "Цепной меч";
+        public override string Description => "Cамый распространенный вид цепного оружия";
+        public override decimal? Price => null;
+        public override Flag<BuyGroup> Group => new Flag<BuyGroup>();
+        public override string Identifier => "spacemarine/chainsword";
+        public override StatsEffect Effect => null;
+
+        public override void OnUse(User user, ItemInfo info)
+        {
+            var room = user.RoomManager.GetRoom();
+            if (room is IMonster monsterRoom)
+            {
+                var bleeding = (int?) (Serializable.Int) GetItemVariables(user).Get("bleeding") ?? 0;
+
+                if (bleeding != 0)
+                {
+                    user.MessageManager.SendMessage(new SentMessage
+                    {
+                        Text = "У монстра не хватает конечностей и он истекает кровью"
+                    });
+
+                    monsterRoom.MakeDamage(user, 10m * bleeding);
+                }
+
+                if (user.Info.ChangeStats(StatsProperty.Stamina, -10))
+                {
+                    user.MessageManager.SendMessage(new SentMessage
+                    {
+                        Text =
+                            "Цепной меч издает сердитое жужжание, перерастающее в пронзительный визг, когда цепь начинает вгрызаться в врага."
+                    });
+                    monsterRoom.MakeDamage(user, user.Info.CurrentStats.Effect[StatsProperty.Strength] + 80);
+                    GetItemVariables(user).Set("bleeding", new Serializable.Int(bleeding + 1));
+                }
+                else
+                {
+                    user.MessageManager.SendMessage(new SentMessage
+                    {
+                        Text = "Ты не смог поднять массивный меч."
+                    });
+                }
+            }
+            else
+            {
+                user.MessageManager.SendMessage(new SentMessage
+                {
+                    Text = "Ты хотел кого-нибудь ударить, но не смог найти подходящих монстров."
+                });
+            }
+        }
+
+        public override bool CanUse(User user, ItemInfo info)
+        {
+            return user.RoomManager.GetRoom() is IMonster;
+        }
+
+        public override void OnEnter(User user, ItemInfo info)
+        {
+            GetItemVariables(user).Set("bleeding", new Serializable.Int(0));
+        }
+
+        public override void OnLeave(User user, ItemInfo info)
+        {
+            GetItemVariables(user).Set("bleeding", new Serializable.Int(0));
+        }
+    }
+}
---
+++ Content/Rooms/SpaceMarine.cs
@@ -0,0 +1,38 @@
+﻿using AdventureBot.Item;
+using AdventureBot.Room;
+using AdventureBot.User;
+
+namespace Content.Rooms
+{
+    // Disabled
+    [Room("monster/SpaceMarine")]
+    public class SpaceMarine : MonsterBase
+    {
+        public override string Name => "Космодесантник";
+        public override string Identifier => "monster/SpaceMarine";
+        public override decimal Health => 1000;
+
+        public override decimal GetDamage(User user)
+        {
+            return user.Random.Next(40, 50);
+        }
+
+        public override void Enter(User user, string[][] buttons)
+        {
+            SendMessage(user,
+                "Перед тобой стоит солдат в тяжелой броне с двухглавым орлом на груди и мечом-пилой в руке", buttons);
+            SendMessage(user, "— Славься Император!", buttons);
+        }
+
+        public override bool OnRunaway(User user)
+        {
+            return true;
+        }
+
+        public override void OnWon(User user)
+        {
+            SendMessage(user, "Победив космодесантника, ты подобрал *цепной меч*");
+            user.ItemManager.Add(new ItemInfo("spacemarine/chainsword", 1));
+        }
+    }
+}
