6478433a3389a6541f5b59e6a5a552e574010773
Ilia Konnov <iliakonnov@gmail.com>
2018-07-16 15:42:07 +0300

    New monster

---
+++ Content/Items/AnimeBadge.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using AdventureBot;
+using AdventureBot.Item;
+using AdventureBot.ObjectManager;
+using AdventureBot.User;
+using AdventureBot.User.Stats;
+
+namespace Content.Items
+{
+    [Item("anime/badge")]
+    public class AnimeBadge : ItemBase
+    {
+        public override Flag<BuyGroup> Group => new Flag<BuyGroup>();
+        public override string Name => "Значок \"I ❤️ 宮崎 駿\"";
+        public override string Description => "";
+        public override decimal? Price => null;
+        public override string Identifier => "anime/badge";
+        public override StatsEffect Effect => new StatsEffect(ChangeType.Set, new Dictionary<StatsProperty, decimal>()
+        {
+            {StatsProperty.Intelligence, 1}
+        });
+        public override bool CanUse(User user, ItemInfo info) => false;
+    }
+}
---
+++ Content/Rooms/Anime.cs
@@ -0,0 +1,35 @@
+﻿using AdventureBot.Item;
+using AdventureBot.Messenger;
+using AdventureBot.Room;
+using AdventureBot.User;
+
+namespace Content.Rooms
+{
+    [Available("monster/anime")]
+    public class Anime : MonsterBase
+    {
+        public override string Name => "Анимешник";
+        public override string Identifier => "monster/anime";
+        public override decimal Health => 150;
+        
+        public override decimal GetDamage(User user)
+        {
+            return 15;
+        }
+
+        public override void Enter(User user, string[][] buttons)
+        {
+            SendMessage(user, "— Это не просто мультики! Это искусство!", buttons);
+        }
+
+        public override bool OnRunaway(User user)
+        {
+            return true;
+        }
+
+        public override void OnWon(User user)
+        {
+            user.ItemManager.Add(new ItemInfo("anime/badge", 1));
+        }
+    }
+}
