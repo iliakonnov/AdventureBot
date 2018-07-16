0cd4e459ebf67c84c32c82183fe60a860fafcd22
Ilia Konnov <iliakonnov@gmail.com>
2018-07-16 17:00:02 +0300

    New monster: Camper.

---
+++ Content/Items/AWP.cs
@@ -0,0 +1,56 @@
+﻿using AdventureBot;
+using AdventureBot.Item;
+using AdventureBot.Messenger;
+using AdventureBot.ObjectManager;
+using AdventureBot.Room;
+using AdventureBot.User;
+using AdventureBot.User.Stats;
+
+namespace Content.Items
+{
+    [Item("camper/awp")]
+    public class AWP : ItemBase
+    {
+        public override Flag<BuyGroup> Group => new Flag<BuyGroup>();
+        public override string Name => "AWP";
+        public override string Description => string.Empty;
+        public override decimal? Price => null;
+        public override string Identifier => "camper/awp";
+        public override StatsEffect Effect => null;
+
+        public override bool CanUse(User user, ItemInfo info)
+        {
+            return user.RoomManager.GetRoom() is IMonster;
+        }
+
+        public override void OnUse(User user, ItemInfo info)
+        {
+            if (!(user.RoomManager.GetRoom() is IMonster monster)) return;
+
+            var bullets = user.ItemManager.Get("bullet")?.Count;
+            if (bullets == null)
+            {
+                user.MessageManager.SendMessage(new SentMessage
+                {
+                    Text = "Ты попытался выстрелить, но оказалось, что у тебя нету патронов"
+                });
+                return;
+            }
+
+            if (!user.ItemManager.Remove(new ItemInfo("bullet", 1)))
+            {
+                user.MessageManager.SendMessage(new SentMessage
+                {
+                    Text = "Почему-то тебе не хватило патронов. Странно."
+                });
+                return;
+            }
+
+            monster.MakeDamage(user, 150m);
+            user.MessageManager.SendMessage(new SentMessage
+            {
+                Text = "Ты всадил пулю в монстра"
+            });
+        }
+    }
+}
---
+++ Content/Rooms/Camper.cs
@@ -0,0 +1,34 @@
+﻿using AdventureBot.Item;
+using AdventureBot.Room;
+using AdventureBot.User;
+
+namespace Content.Rooms
+{
+    [Available("monster/camper")]
+    public class Camper : MonsterBase
+    {
+        public override string Name => "Кэмпер";
+        public override string Identifier => "monster/camper";
+        public override decimal Health => 70;
+
+        public override decimal GetDamage(User user)
+        {
+            return 70;
+        }
+
+        public override void Enter(User user, string[][] buttons)
+        {
+            SendMessage(user, "Сидел за ящиками, падла. Задай ему жару!", buttons);
+        }
+
+        public override bool OnRunaway(User user)
+        {
+            return true;
+        }
+
+        public override void OnWon(User user)
+        {
+            user.ItemManager.Add(new ItemInfo("camper/awp", 1));
+        }
+    }
+}
