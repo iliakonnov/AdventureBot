77dae45b0073adcb8ce0017c13707750f47f2ef0
Ilia Konnov <iliakonnov@gmail.com>
2018-07-17 19:04:02 +0300

    New room: Skitties

---
+++ Content/Items/Skitties.cs
@@ -0,0 +1,29 @@
+﻿using AdventureBot;
+using AdventureBot.Item;
+using AdventureBot.ObjectManager;
+using AdventureBot.User;
+using AdventureBot.User.Stats;
+
+namespace Content.Items
+{
+    [Item("skitties/candy")]
+    public class Skitties : ItemBase
+    {
+        public override Flag<BuyGroup> Group => new Flag<BuyGroup>();
+        public override string Name => "Конфета \"Skitties\"";
+        public override string Description => string.Empty;
+        public override decimal? Price => 50;
+        public override string Identifier => "skitties/candy";
+        public override StatsEffect Effect => null;
+        
+        public override bool CanUse(User user, ItemInfo info)
+        {
+            return true;
+        }
+
+        public override void OnUse(User user, ItemInfo info)
+        {
+            user.Info.ChangeStats(StatsProperty.Health, 25);
+        }
+    }
+}
---
+++ Content/Rooms/Skitties.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using AdventureBot;
+using AdventureBot.Item;
+using AdventureBot.Messenger;
+using AdventureBot.Room;
+using AdventureBot.User;
+
+namespace Content.Rooms
+{
+    [Available("room/skitties", Difficulity.Any)]
+    public class Skitties : RoomBase
+    {
+        public Skitties()
+        {
+            Buttons = new NullableDictionary<MessageRecived, Dictionary<string, MessageRecived>>
+            {
+                {
+                    null, new Dictionary<string, MessageRecived>
+                    {
+                        {
+                            "Попробовать", (user, message) =>
+                            {
+                                SendMessage(user,
+                                    "Вполне себе обычные вкусненькие конфеты. Правда, ты не заметил, как съел полведра и теперь чувствуешь, как содержание глюкозы в твоей крови достигло критической отметки. Кажется, у тебя диабет. Но есть и плюсы: отойдя на метров двести, ты обнаружил у себя в кармане конфеты \"Skitties\". Да-да, те самые.");
+                                user.ItemManager.Add(new ItemInfo("skitties/candy", 10));
+                                user.RoomManager.Leave();
+                            }
+                        },
+                        {
+                            "Нет, спасибо", (user, message) =>
+                            {
+                                SendMessage(user,
+                                    "— Ну ладно дружище, дело твое! — после этих слов он подобрал ведро, сел на жирафа и ускакал на Юго-Запад");
+                                user.RoomManager.Leave();
+                            }
+                        }
+                    }
+                }
+            };
+        }
+
+        public override string Name => "?";
+        public override string Identifier => "room/skitties";
+
+        public override void OnEnter(User user)
+        {
+            SendMessage(user,
+                "Ты увидел афроамериканца, который доил жирафа конфетами, ел их и угарал как укуренный. Ты начал было мысленно перечислять все галюциногенные грибы, которые могли быть подмешаны в зелье, но темнокожий предложил тебе этих самых конфет. Что будем делать?",
+                GetButtons(user));
+        }
+
+        public override bool OnLeave(User user)
+        {
+            return true;
+        }
+
+        public override void OnMessage(User user, RecivedMessage message)
+        {
+            HandleButtonAlways(user, message);
+        }
+    }
+}
