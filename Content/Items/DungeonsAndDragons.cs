304985850118a7f803369f3e034c24464ce6398b
Ilia Konnov <iliakonnov@gmail.com>
2018-07-17 19:14:41 +0300

    New monster: Geek

---
+++ Content/Items/DungeonsAndDragons.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using AdventureBot;
+using AdventureBot.Item;
+using AdventureBot.ObjectManager;
+using AdventureBot.User;
+using AdventureBot.User.Stats;
+
+namespace Content.Items
+{
+    [Item("geek/d&d")]
+    public class DungeonsAndDragons : ItemBase
+    {
+        public override Flag<BuyGroup> Group => new Flag<BuyGroup>();
+        public override string Name => "Dungeons and Dragons";
+        public override string Description => string.Empty;
+        public override decimal? Price => null;
+        public override string Identifier => "geek/d&d";
+
+        public override StatsEffect Effect => new StatsEffect(ChangeType.Add, new Dictionary<StatsProperty, decimal>
+        {
+            {StatsProperty.Intelligence, 10}
+        });
+
+        public override bool CanUse(User user, ItemInfo info)
+        {
+            return false;
+        }
+    }
+}
---
+++ Content/Rooms/Geek.cs
@@ -0,0 +1,61 @@
+﻿using System.Linq;
+using AdventureBot.Item;
+using AdventureBot.Messenger;
+using AdventureBot.Room;
+using AdventureBot.User;
+
+namespace Content.Rooms
+{
+    [Available("monster/geek", Difficulity.Medium)]
+    public class Geek : MonsterBase
+    {
+        public override string Name => "Гик";
+        public override string Identifier => "monster/geek";
+        public override decimal Health => 150;
+
+        public override decimal GetDamage(User user)
+        {
+            return 25;
+        }
+
+        protected override string[][] GetActions(User user)
+        {
+            return base.GetActions(user)
+                .Concat(new[] {new[] {"А когда HL-3 выйдет?"}})
+                .ToArray();
+        }
+
+        public override void Enter(User user, string[][] buttons)
+        {
+            SendMessage(user,
+                "\"...Ты в С++ делал хоть клик? Ты хоть в один фандом вник? Знаешь эльфийский язык? Хотя бы english can you speak?\"");
+            SendMessage(user,
+                "Да, эта бурная смесь из тонны ненужной информации, предрассудков и бесконечного неуважения к псевдо-гикам умеет внушать страх",
+                buttons);
+        }
+
+        public override void OnMessage(User user, RecivedMessage message)
+        {
+            if (message.Text == "А когда HL-3 выйдет?")
+            {
+                SendMessage(user,
+                    "За толстыми линзами очков блеснули слезы. Гик пошатнулся, рухнул на пол, свернулся калачиком и тихонько заплакал. Вокруг него образовалась небольшая лужа. Думаю, нам пора оставить его наедине с собой на некоторое время");
+                user.RoomManager.Leave();
+            }
+            else
+            {
+                base.OnMessage(user, message);
+            }
+        }
+
+        public override bool OnRunaway(User user)
+        {
+            return true;
+        }
+
+        public override void OnWon(User user)
+        {
+            user.ItemManager.Add(new ItemInfo("geek/D&D", 1));
+        }
+    }
+}
