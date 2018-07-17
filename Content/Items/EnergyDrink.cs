76ce4707c947a7dd8e284bf138372e3ef63dd560
Ilia Konnov <iliakonnov@gmail.com>
2018-07-17 18:28:41 +0300

    New item: Energy drink

---
+++ Content/Items/EnergyDrink.cs
@@ -0,0 +1,29 @@
+﻿using AdventureBot;
+using AdventureBot.Item;
+using AdventureBot.ObjectManager;
+using AdventureBot.User;
+using AdventureBot.User.Stats;
+
+namespace Content.Items
+{
+    [Item("item/energy")]
+    public class EnergyDrink : ItemBase
+    {
+        public override Flag<BuyGroup> Group => new Flag<BuyGroup>(BuyGroup.Market);
+        public override string Name => "Энергетик";
+        public override string Description => "Для тех, кто хочет жить опасно";
+        public override decimal? Price => 100;
+        public override string Identifier => "item/energy";
+        public override StatsEffect Effect => null;
+
+        public override bool CanUse(User user, ItemInfo info)
+        {
+            return true;
+        }
+
+        public override void OnUse(User user, ItemInfo info)
+        {
+            user.Info.ChangeStats(StatsProperty.Stamina, user.Info.MaxStats.Effect[StatsProperty.Stamina]);
+        }
+    }
+}
