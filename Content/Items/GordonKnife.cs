2c7d27edec4b8ef7b563f809be9e4948e2bf0c6e
Ilia Konnov <iliakonnov@gmail.com>
2018-07-17 19:22:18 +0300

    New monster: Gordon Ramsey

---
+++ Content/Items/GordonKnife.cs
@@ -0,0 +1,46 @@
+﻿using AdventureBot;
+using AdventureBot.Item;
+using AdventureBot.Messenger;
+using AdventureBot.ObjectManager;
+using AdventureBot.Room;
+using AdventureBot.User;
+using AdventureBot.User.Stats;
+
+namespace Content.Items
+{
+    [Item("gordonRamsey/knife")]
+    public class GordonKnife : ItemBase
+    {
+        public override Flag<BuyGroup> Group => new Flag<BuyGroup>();
+        public override string Name => "Нож Шефа";
+        public override string Description => string.Empty;
+        public override decimal? Price => null;
+        public override string Identifier => "gordonRamsey/knife";
+        public override StatsEffect Effect => null;
+
+        public override bool CanUse(User user, ItemInfo info)
+        {
+            return user.RoomManager.GetRoom() is IMonster;
+        }
+
+        public override void OnUse(User user, ItemInfo info)
+        {
+            if (!(user.RoomManager.GetRoom() is IMonster monster))
+            {
+                return;
+            }
+
+            if (user.Info.ChangeStats(StatsProperty.Stamina, 15))
+            {
+                monster.MakeDamage(user, user.Info.CurrentStats.Effect[StatsProperty.Strength] + 15);
+            }
+            else
+            {
+                user.MessageManager.SendMessage(new SentMessage
+                {
+                    Text = "Ты слишком устал"
+                });
+            }
+        }
+    }
+}
---
+++ Content/Rooms/GordonRamsey.cs
@@ -0,0 +1,34 @@
+﻿using AdventureBot.Item;
+using AdventureBot.Room;
+using AdventureBot.User;
+
+namespace Content.Rooms
+{
+    [Available("monster/gordonRamsey", Difficulity.Medium)]
+    public class GordonRamsey : MonsterBase
+    {
+        public override string Name => "Гордон Рамзи";
+        public override string Identifier => "monster/gordonRamsey";
+        public override decimal Health => 200;
+
+        public override decimal GetDamage(User user)
+        {
+            return 15;
+        }
+
+        public override void Enter(User user, string[][] buttons)
+        {
+            SendMessage(user, "Эта свинина настолько жирная, что уже натягивает свои леопардовые лосины!", buttons);
+        }
+
+        public override bool OnRunaway(User user)
+        {
+            return true;
+        }
+
+        public override void OnWon(User user)
+        {
+            user.ItemManager.Add(new ItemInfo("gordonRamsey/knife", 1));
+        }
+    }
+}
