a8e69b83a49a988182daec97b40b77130ad9b58a
Ilia Konnov <iliakonnov@gmail.com>
2018-07-18 15:10:37 +0300

    New room: Legolas

---
+++ Content/Items/Bow.cs
@@ -0,0 +1,35 @@
+﻿using AdventureBot;
+using AdventureBot.Item;
+using AdventureBot.ObjectManager;
+using AdventureBot.Room;
+using AdventureBot.User;
+using AdventureBot.User.Stats;
+
+namespace Content.Items
+{
+    [Item("legolas/bow")]
+    public class Bow : ItemBase
+    {
+        public override Flag<BuyGroup> Group => new Flag<BuyGroup>();
+        public override string Name => "Эльфийский лук";
+        public override string Description => "Этому луку стрелы не нужны!";
+        public override decimal? Price => null;
+        public override string Identifier => "legolas/bow";
+        public override StatsEffect Effect => null;
+        
+        public override bool CanUse(User user, ItemInfo info)
+        {
+            return user.RoomManager.GetRoom() is IMonster;
+        }
+
+        public override void OnUse(User user, ItemInfo info)
+        {
+            if (!(user.RoomManager.GetRoom() is IMonster monster))
+            {
+                return;
+            }
+            
+            monster.MakeDamage(user, 300);
+        }
+    }
+}
---
+++ Content/Rooms/Legolas.cs
@@ -0,0 +1,37 @@
+﻿using AdventureBot.Item;
+using AdventureBot.Messenger;
+using AdventureBot.Room;
+using AdventureBot.User;
+
+namespace Content.Rooms
+{
+    [Available("monster/legolas", Difficulity.Hard)]
+    public class Legolas : MonsterBase
+    {
+        public override string Name => "Леголас";
+        public override string Identifier => "monster/legolas";
+        public override decimal Health => 500;
+
+        public override decimal GetDamage(User user)
+        {
+            return 70;
+        }
+
+        public override void Enter(User user, string[][] buttons)
+        {
+            SendMessage(user,
+                "Никогда не промахивается и может глушить эль галлонами не пьянея.  Стрела пролетела в миллиметре от твоей макушки.");
+            SendMessage(user, "— Это был предупредительный выстрел!", buttons);
+        }
+
+        public override bool OnRunaway(User user)
+        {
+            return true;
+        }
+
+        public override void OnWon(User user)
+        {
+            user.ItemManager.Add(new ItemInfo("legolas/bow", 1));
+        }
+    }
+}
