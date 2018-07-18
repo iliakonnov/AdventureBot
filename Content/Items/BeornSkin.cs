669e0b43642b1490d0bd1e60266390c0a699553b
Ilia Konnov <iliakonnov@gmail.com>
2018-07-18 10:06:17 +0300

    New room: Beorn

---
+++ Content/Items/BeornSkin.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using AdventureBot;
+using AdventureBot.Item;
+using AdventureBot.ObjectManager;
+using AdventureBot.User;
+using AdventureBot.User.Stats;
+
+namespace Content.Items
+{
+    [Item("beorn/skin")]
+    public class BeornSkin : ItemBase
+    {
+        public override Flag<BuyGroup> Group => new Flag<BuyGroup>();
+        public override string Name => "Шкура беорна";
+        public override string Description => "Крепкая";
+        public override decimal? Price => null;
+        public override string Identifier => "beorn/skin";
+
+        public override StatsEffect Effect => new StatsEffect(ChangeType.Add, new Dictionary<StatsProperty, decimal>
+        {
+            {StatsProperty.Health, 20},
+            {StatsProperty.Defence, 20}
+        });
+
+        public override bool CanUse(User user, ItemInfo info)
+        {
+            return false;
+        }
+    }
+}
---
+++ Content/Rooms/Beorn.cs
@@ -0,0 +1,38 @@
+﻿using AdventureBot.Item;
+using AdventureBot.Room;
+using AdventureBot.User;
+
+namespace Content.Rooms
+{
+    [Available("monster/beorn", Difficulity.Hard)]
+    public class Beorn : MonsterBase
+    {
+        public override string Name => "Беорн";
+        public override string Identifier => "monster/beorn";
+        public override decimal Health => 1500;
+
+        public override decimal GetDamage(User user)
+        {
+            return 50;
+        }
+
+        public override void Enter(User user, string[][] buttons)
+        {
+            SendMessage(
+                user,
+                "Этот огромный человекоподобный медведь — когда-то одно из самых опасных существ в Средиземье. Говорят, его когти настолько остры, что можно поцарапаться, просто взглянув на них, но пруфа нет. Пора проверить!",
+                buttons
+            );
+        }
+
+        public override bool OnRunaway(User user)
+        {
+            return true;
+        }
+
+        public override void OnWon(User user)
+        {
+            user.ItemManager.Add(new ItemInfo("beorn/skin", 1));
+        }
+    }
+}
