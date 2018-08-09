46afc1036ba1996d974f544f78e55a5ec2fc88cb
Ilia Konnov <iliakonnov@gmail.com>
2018-08-09 12:44:35 +0300

    Add templates for new items and monsters

---
+++ Content/Items/ItemTemplate.cs
@@ -0,0 +1,55 @@
+﻿#if false // Этот файл не должен компилироваться, он только в качестве шаблона
+
+using System.Collections.Generic;
+using AdventureBot;
+using AdventureBot.Item;
+using AdventureBot.ObjectManager;
+using AdventureBot.User;
+using AdventureBot.User.Stats;
+
+namespace Content.Items
+{
+    [Item(Id)]
+    public class ItemTemplate : ItemBase
+    {
+        public const string Id = "item/Какой-то уникальный идентификатор";
+
+        // Где предмет можно купить.
+        public override StructFlag<BuyGroup> Group =>
+            new StructFlag<BuyGroup>(BuyGroup.Guild, BuyGroup.Gym, BuyGroup.Market, BuyGroup.Merchant);
+
+        public override string Name => "Название вещи";
+        public override string Description => "Её описание";
+
+        public override decimal? Price => 1234; // Стоимость одной штуки.
+
+        // public override decimal? Price => null;  // Если вещь не продается, то написать так.
+        public override string Identifier => Id;
+
+        // Какие эффекты дает предмет, когда активирован
+        public override StatsEffect Effect => new StatsEffect(
+            // Каким образом он влияет на статы.
+            // Если прибавляет, то Add. Если домножает, то Mulitply. Если жестко устанавливает значение, то Set
+            ChangeType.Add,
+
+            // На какие статы и насколько он влияет. Всё лишнее нужно просто убрать
+            new Dictionary<StatsProperty, decimal>
+            {
+                {StatsProperty.Defence, 100},
+                {StatsProperty.Health, 100},
+                {StatsProperty.Intelligence, 100},
+                {StatsProperty.Karma, 100},
+                {StatsProperty.Mana, 100},
+                {StatsProperty.Stamina, 100},
+                {StatsProperty.Strength, 100}
+            }
+        );
+
+        public override bool CanUse(User user, ItemInfo info)
+        {
+            return false; // Если предмет можно использовать, то true. Если нельзя, то false
+        }
+    }
+}
+
+#endif
---
+++ Content/Rooms/MonsterTemplate.cs
@@ -0,0 +1,48 @@
+﻿#if false // Этот файл не должен компилироваться, он только в качестве шаблона
+
+using AdventureBot.Item;
+using AdventureBot.Room;
+using AdventureBot.User;
+using AdventureBot.User.Stats;
+
+namespace Content.Rooms
+{
+    // Тут задается уровень сложности. Список доступных значений см. в RoomManager.Difficulity
+    [Available(Id, Difficulity.None)]
+    public class MonsterTemplate : MonsterBase
+    {
+        public const string Id = "monster/Какой-то уникальный идентификатор";
+        public override string Name => "Имя этой локации";
+        public override string Identifier => Id;
+        protected override decimal Health => 12345; // Сколько здоровья у монстра
+
+        protected override decimal GetDamage(User user)
+        {
+            return 12345; // Какой урон он наносит
+        }
+
+        protected override void Enter(User user, string[][] buttons)
+        {
+            SendMessage(user, "Сообщение при входе к монстру", buttons);
+            SendMessage(user, "Их может быть несколько =)", buttons);
+        }
+
+        protected override bool OnRunaway(User user)
+        {
+            SendMessage(user, "Сообщение при побеге от монстра. Если сообщения нет, то просто удалить эту строчку");
+            return true; // Заменить true на false, чтобы нельзя было сбежать
+        }
+
+        protected override void OnWon(User user)
+        {
+            SendMessage(user, "Сообщение при победе");
+            // Чтобы дать несколько разных вещей, надо просто продублировать строчку нижу несколько раз =)
+            user.ItemManager.Add(new ItemInfo("Идентификатор вещи, которую надо дать", 1234)); // 1234 -- кол-во
+
+            user.Info.Gold += 12345; // Сколько золота дать игроку. Если золота не надо, то удалить строчку
+            user.Info.ChangeStats(StatsProperty.Karma, 12345); // То же самое, что и с золотом, но только карма
+        }
+    }
+}
+
+#endif
