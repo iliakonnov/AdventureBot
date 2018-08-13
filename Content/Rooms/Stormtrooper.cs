99310d5e8ca3f7c9a7c0123f980fea21bde1cea3
Ilia Konnov <iliakonnov@gmail.com>
2018-08-13 11:50:39 +0300

    New monster: Stormtrooper. Close #22

---
+++ Content/Rooms/Stormtrooper.cs
@@ -0,0 +1,37 @@
+﻿using AdventureBot.Room;
+using AdventureBot.User;
+
+namespace Content.Rooms
+{
+    [Available("monster/stormtrooper", Difficulity.Lower)]
+    public class Stormtrooper : MonsterBase
+    {
+        public override string Name => "Штурмовик";
+        public override string Identifier => "monster/stormtrooper";
+        protected override decimal Health => 40;
+
+        protected override decimal GetDamage(User user)
+        {
+            return user.Random.NextDouble() > 0.35
+                ? 10
+                : 0;
+        }
+
+        protected override void Enter(User user, string[][] buttons)
+        {
+            SendMessage(user,
+                "Солдат Новой Империи, покрытый белой пластмассовой броней, наводит на тебя свое лазерное оружие. Вернее, это он так думает.",
+                buttons);
+        }
+
+        protected override bool OnRunaway(User user)
+        {
+            return true;
+        }
+
+        protected override void OnWon(User user)
+        {
+            user.Info.Gold += 25;
+        }
+    }
+}
