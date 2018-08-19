986799b302697ab3178fae399000c352e597a812
Ilia Konnov <iliakonnov@gmail.com>
2018-08-19 14:12:44 +0300

    Fix and reformat Church

--- Content/Rooms/Church.cs
+++ Content/Rooms/Church.cs
@@ -2,6 +2,7 @@ using AdventureBot.Messenger;
 using AdventureBot.Room;
 using AdventureBot.Room.BetterRoom;
 using AdventureBot.User;
+using AdventureBot.User.Stats;
 
 namespace Content.Rooms
 {
@@ -21,15 +22,17 @@ namespace Content.Rooms
         {
             base.OnEnter(user);
             SwitchAction<MainHandler>(user);
-            
+
             if (user.Info.CurrentStats.GetStat(StatsProperty.Karma) < 0)
             {
                 SendMessage(user, "Даже тут не любят таких отщепенцев, как ты.");
                 user.RoomManager.Leave();
                 return;
             }
-            
-            SendMessage(user, "Пастафарианский храм! Ты чуть не задохнулся от жары, производимой электроплитками, на которых варилась паста и жарились тефтельки. С потолка свисает огромное количество различных приспособлений для варки макарон: дуршлаги, кастрюли, лопатки, щипцы. А по полу вперемешку разбросаны тарелки с остывающей пастой и мешки со спагетти, макаронами и ригаттой.", GetButtons(user));
+
+            SendMessage(user,
+                "Пастафарианский храм! Ты чуть не задохнулся от жары, производимой электроплитками, на которых варилась паста и жарились тефтельки. С потолка свисает огромное количество различных приспособлений для варки макарон: дуршлаги, кастрюли, лопатки, щипцы. А по полу вперемешку разбросаны тарелки с остывающей пастой и мешки со спагетти, макаронами и ригаттой.",
+                GetButtons(user));
         }
 
         [Action]
@@ -43,19 +46,14 @@ namespace Content.Rooms
             public void OnDonat(User user, RecivedMessage message)
             {
                 Room.SwitchAction<Donat>(user);
-                Room.SendMessage(user, "Ты решил пожертвовать Макаронному Монстру немного своих сбережений.", Room.GetButtons(user));
+                Room.SendMessage(user, "Ты решил пожертвовать Макаронному Монстру немного своих сбережений.",
+                    Room.GetButtons(user));
             }
 
             [Button("Уйти")]
             public void Leave(User user, RecivedMessage message)
             {
                 user.RoomManager.Leave();
-            }
-
-            [Fallback]
-            public void Fallback(User user, RecivedMessage message)
-            {
-                Room.SendMessage(user, "Echo: " + message.Text, Room.GetButtons(user));
             }
         }
 
@@ -69,62 +67,69 @@ namespace Content.Rooms
             [Button("150")]
             public void DonatMinium(User user, RecivedMessage message)
             {
-                if(user.Info.TryDecreaseGold(150))
+                if (user.Info.TryDecreaseGold(150))
                 {
-                    user.Info.ChangeStats(StatsProperty.Karma, 1)
-                    Room.SendMessage(user, "Ты отсыпал пару золотых монет на блюдечко. Почти моментально они испарились с легким синим дымком, и ты почувствовал что совершил ещё одно доброе дело.", Room.GetButtons(user));
+                    user.Info.ChangeStats(StatsProperty.Karma, 1);
+                    Room.SendMessage(user,
+                        "Ты отсыпал пару золотых монет на блюдечко. Почти моментально они испарились с легким синим дымком, и ты почувствовал что совершил ещё одно доброе дело.",
+                        Room.GetButtons(user));
                 }
                 else
                 {
-                    Room.SendMessage(user, "Ты отсыпал пару золотых монет на блюдечко. Они начали было испаряться, но вдруг затормозили и отскочили обратно. Думаю, у тебя недостаточно золота.", Room.GetButtons(user));
+                    Room.SendMessage(user,
+                        "Ты отсыпал пару золотых монет на блюдечко. Они начали было испаряться, но вдруг затормозили и отскочили обратно. Думаю, у тебя недостаточно золота.",
+                        Room.GetButtons(user));
                 }
             }
 
             [Button("750")]
             public void Donat750(User user, RecivedMessage message)
             {
-                if(user.Info.TryDecreaseGold(750))
+                if (user.Info.TryDecreaseGold(750))
                 {
-                    user.Info.ChangeStats(StatsProperty.Karma, 5)
+                    user.Info.ChangeStats(StatsProperty.Karma, 5);
                     user.Info.ChangeStats(StatsProperty.Health, user.Info.MaxStats.GetStat(StatsProperty.Health), true);
-                    Room.SendMessage(user, "Ты отсыпал горсть золотых монет на блюдечко. Почти моментально они испарились с легким синим дымком, и ты почувствовал что совершил ещё одно доброе дело.");
-                    Room.SendMessage(user, "За такое щедрое пожертвование служители храма накормили тебя вкусной пастой.", Room.GetButtons(user));
+                    Room.SendMessage(user,
+                        "Ты отсыпал горсть золотых монет на блюдечко. Почти моментально они испарились с легким синим дымком, и ты почувствовал что совершил ещё одно доброе дело.");
+                    Room.SendMessage(user,
+                        "За такое щедрое пожертвование служители храма накормили тебя вкусной пастой.",
+                        Room.GetButtons(user));
                 }
                 else
                 {
-                    Room.SendMessage(user, "Ты отсыпал горсть золотых монет на блюдечко. Они начали было испаряться, но вдруг затормозили и отскочили обратно. Думаю, у тебя недостаточно золота.", Room.GetButtons(user));
+                    Room.SendMessage(user,
+                        "Ты отсыпал горсть золотых монет на блюдечко. Они начали было испаряться, но вдруг затормозили и отскочили обратно. Думаю, у тебя недостаточно золота.",
+                        Room.GetButtons(user));
                 }
             }
-            
+
             [Button("1500")]
             public void DonatMaximum(User user, RecivedMessage message)
             {
-                if(user.Info.TryDecreaseGold(1500))
+                if (user.Info.TryDecreaseGold(1500))
                 {
-                    user.Info.ChangeStats(StatsProperty.Karma, 10)
+                    user.Info.ChangeStats(StatsProperty.Karma, 10);
                     user.Info.ChangeStats(StatsProperty.Health, user.Info.MaxStats.GetStat(StatsProperty.Health), true);
                     user.Info.ChangeStats(StatsProperty.Defence, 5);
-                    Room.SendMessage(user, "Ты отсыпал горсть золотых монет на блюдечко. Почти моментально они испарились с легким синим дымком, и ты почувствовал что совершил ещё одно доброе дело.");
-                    Room.SendMessage(user, "За такое щедрое пожертвование служители храма накормили тебя вкусной пастой и обучили секретам защиты.", Room.GetButtons(user));
+                    Room.SendMessage(user,
+                        "Ты отсыпал горсть золотых монет на блюдечко. Почти моментально они испарились с легким синим дымком, и ты почувствовал что совершил ещё одно доброе дело.");
+                    Room.SendMessage(user,
+                        "За такое щедрое пожертвование служители храма накормили тебя вкусной пастой и обучили секретам защиты.",
+                        Room.GetButtons(user));
                 }
                 else
                 {
-                    Room.SendMessage(user, "Ты отсыпал горсть золотых монет на блюдечко. Они начали было испаряться, но вдруг затормозили и отскочили обратно. Думаю, у тебя недостаточно золота.", Room.GetButtons(user));
+                    Room.SendMessage(user,
+                        "Ты отсыпал горсть золотых монет на блюдечко. Они начали было испаряться, но вдруг затормозили и отскочили обратно. Думаю, у тебя недостаточно золота.",
+                        Room.GetButtons(user));
                 }
             }
-            
+
             [Button("Уйти")]
             public void Leave(User user, RecivedMessage message)
             {
                 user.RoomManager.Leave();
             }
-
-            [Fallback]
-            public void Fallback(User user, RecivedMessage message)
-            {
-                Room.SendMessage(user, "Echo (asdf): " + message.Text, Room.GetButtons(user));
-            }
         }
-
     }
 }
