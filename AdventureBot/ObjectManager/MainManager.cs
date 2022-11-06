250d254e79e352266aa57a25077281d5174c3cfc
Ilia Konnov <ilia@sldr.xyz>
2022-11-06 18:04:22 +0300

    Emit better error messages when failed loading python

--- AdventureBot/ObjectManager/MainManager.cs
+++ AdventureBot/ObjectManager/MainManager.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 using IronPython.Hosting;
@@ -8,6 +9,7 @@ using IronPython.Runtime;
 using IronPython.Runtime.Types;
 using Microsoft.Scripting.Hosting;
 using Microsoft.Scripting.Runtime;
+using NLog;
 
 namespace AdventureBot.ObjectManager;
 
@@ -19,12 +21,13 @@ internal interface IObjectManager
 internal class MainManager : Singleton<MainManager>
 {
     private readonly List<IObjectManager> _managers = new();
+    private static Logger Logger = LogManager.GetCurrentClassLogger();
 
     internal void Register<TMgr>(TMgr manager) where TMgr : IObjectManager
     {
         _managers.Add(manager);
     }
-    
+
     private void LoadType(Type type)
     {
         if (!(type.GetCustomAttribute(typeof(GameObjectAttribute)) is GameObjectAttribute attr))
@@ -50,25 +53,24 @@ internal class MainManager : Singleton<MainManager>
 
     internal void LoadAssembly(string path)
     {
+        if (!File.Exists(path))
+        {
+            Logger.Warn("Skipping assembly {}: not found", path);
+            return;
+        }
+
         LoadAssembly(Assembly.LoadFrom(path));
     }
 
-    public void LoadPython(string module)
+    private void LoadPythonInternal(ScriptEngine engine, string module)
     {
-        var engine = Python.CreateEngine();
-        engine.Runtime.LoadAssembly(Assembly.GetExecutingAssembly());
-
-        var paths = engine.GetSearchPaths().ToList();
-        paths.AddRange(Configuration.Config.GetSection("python_paths").GetChildren().Select(path => path.Value));
-        engine.SetSearchPaths(paths);
-
         var parts = module.Split('.').ToList();
         parts.Add("__all__");
         using var currPart = parts.GetEnumerator();
 
         currPart.MoveNext();
         var scope = engine.ImportModule(module);
-        
+
         currPart.MoveNext();
         dynamic prev = null;
         var curr = scope.GetVariable(currPart.Current);
@@ -76,26 +78,41 @@ internal class MainManager : Singleton<MainManager>
         while (currPart.MoveNext())
         {
             prev = curr;
-            curr = ((ObjectDictionaryExpando) ((PythonModule) curr).Scope.Storage).Dictionary[currPart.Current];
+            curr = ((ObjectDictionaryExpando)((PythonModule)curr).Scope.Storage).Dictionary[currPart.Current];
         }
 
-        foreach (var name in ((IList<object>) curr).Cast<string>())
+        foreach (var name in ((IList<object>)curr).Cast<string>())
         {
-            var value = ((ObjectDictionaryExpando) ((PythonModule) prev).Scope.Storage).Dictionary[name];
+            var value = ((ObjectDictionaryExpando)((PythonModule)prev).Scope.Storage).Dictionary[name];
             if (value is not PythonType type)
             {
                 continue;
             }
 
-            try
-            {
-                LoadType(type);
-            }
-            catch (Exception exception)
-            {
-                Console.Error.WriteLine(engine.GetService<ExceptionOperations>().FormatException(exception));
-                throw;
-            }
+            LoadType(type);
+        }
+    }
+
+    public void LoadPython(string module)
+    {
+        var engine = Python.CreateEngine();
+        foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
+        {
+            engine.Runtime.LoadAssembly(assembly);
+        }
+
+        var paths = engine.GetSearchPaths().ToList();
+        paths.AddRange(Configuration.Config.GetSection("python_paths").GetChildren().Select(path => path.Value));
+        engine.SetSearchPaths(paths);
+        
+        try
+        {
+            LoadPythonInternal(engine, module);
+        }
+        catch (Exception exception)
+        {
+            Console.Error.WriteLine(engine.GetService<ExceptionOperations>().FormatException(exception));
+            throw;
         }
     }
 }
